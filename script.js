14cf351e9e7c2be959a66c4b4a28fc593a5a5e10
goldencub <133077593+goldencub@users.noreply.github.com>
2025-06-25 13:06:10 +1000

    Testing Different Javascript DOM manipulation methods

---
+++ script.js
@@ -0,0 +1,43 @@
+const promptBox = document.querySelector("#promptBox")
+
+const content = document.createElement("p");
+content.classList.add("content");
+content.textContent = "hey I'm red";
+content.style.color = "red";
+
+const content2 = document.createElement("h2");
+content2.textContent = "I'm Big";
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3!";
+h3.style.color = "blue";
+
+const divTest = document.createElement("div");
+divTest.classList.add("Divtest");
+divTest.style.border = "2px solid black";
+divTest.style.backgroundColor = "pink";
+
+const DivChild = document.createElement("h2");
+DivChild.textContent = "Im in a box";
+
+const DivChild2 = document.createElement("p");
+DivChild2.textContent = "Me too!";
+
+const shufButt = document.querySelector("#shufButt");
+shufButt.addEventListener("click", () => {
+     alert("hello there");
+     alert("still here");
+    });
+
+
+
+
+promptBox.appendChild(content);
+promptBox.appendChild(content2);
+promptBox.appendChild(h3);
+
+divTest.appendChild(DivChild);
+divTest.appendChild(DivChild2);
+promptBox.appendChild(divTest);
+
+
